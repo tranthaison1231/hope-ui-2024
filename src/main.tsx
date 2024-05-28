0a704801aa52aa451865e781ffb10b3c039ceffb
LAPTOP-VVJKGVPT\Anh Loc <hothanhloc2004@gmail.com>
2024-05-28 19:23:30 +0700

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import SignUp from "./components/SignUp.tsx";
+import "./App.css";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <SignUp />
+  </React.StrictMode>
 );
