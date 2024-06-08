804ffa3ce66db90d39b07d9ec867067b4385d30e
LAPTOP-VVJKGVPT\Anh Loc <hothanhloc2004@gmail.com>
2024-06-08 14:22:51 +0700

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 export default function Header() {
   const navigate = useNavigate();
   return (
-    <section className=" flex ">
+    <section className="" >
     <div className="h-20 flex">
       <Button
         onClick={() => {
@@ -17,16 +17,14 @@ export default function Header() {
       </Button>
       </div>
      
-      <div className="flex justify-between">
+      <div className="flex ">
         
         <div className="w-1/2">
         <form className=" pt-5 pl-5"  action="/action_page.php">
-        <input className="w-80 h-9" type="text" placeholder="Search" name="search"/>
+        <input className="w-80 h-9 " type="text" placeholder="Search" name="search"/>
         </form>
         </div>
-
-
-        <div className=" w-1/2 pt-5   ">
+        <div className=" w-3/4 pt-5   ">
         <div className="flex justify-end md:mt-0 space-x-6 rtl:space-x-reverse">
 
            <img src="../src/assets/imguk.png"></img> 
@@ -35,13 +33,12 @@ export default function Header() {
            <img src="../src/assets/avatar.png"></img>
            <h1>Austin Robertson</h1>
            <p>Marketing Administrator</p>  
-        
-           
         </div>
 
+        
 </div>
 </div>
-     
+
 </section>
   );
 }
